ec4e3aab4e528b9d114e2d94c0e34794e5183c24
Vladimir Kalinin <Vofanchic@mail.ru>
2020-10-18 20:20:20 +0500

    10.1

--- 10_3.py
+++ 10_3.py
@@ -1,25 +1,9 @@
 import requests
+import time
+today = str(time.time()).split('.')[0]
+two_d_ago = int(today) - 86400
 
-class YaUploader:
-    def __init__(self, token: str):
-        self.token = token
+response = requests.get("https://api.stackexchange.com/2.2/questions", params={'fromdate': two_d_ago, 'todate': today, 'tagged': 'Python', 'site': 'stackoverflow'})
 
-    def upload(self, file_path: str):
-        file_name = file_path.split('\\')[-1]
-        HEADERS = {"Authorization": f"OAuth {self.token}"}
-        response = requests.get(
-            "https://cloud-api.yandex.net/v1/disk/resources/upload",
-            params={"path": f"{file_name}"},
-            headers=HEADERS)
-
-        try:
-            upload_file = requests.put(response.json()['href'], files = {'file': open(file_path, 'rb')})
-            if upload_file.status_code == 201:
-                return print(f'фаил {file_path} загружен')
-            else:
-                return print(f'ошибка {upload_file.status_code}')
-        except:
-            return print('ошибка, возможно, фаил уже загружен')
-
-uploader = YaUploader('')
-result = uploader.upload(r'c:\my_folder\file.txt')
+for i in response.json()['items']:
+    print(i['title'])
