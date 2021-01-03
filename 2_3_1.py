26616dcfa6526a33e75603e2d3aab7097cfd6643
Vladimir Kalinin <Vofanchic@mail.ru>
2021-01-03 20:15:08 +0500

    2_3_1(2)

--- 2_3_1.py
+++ 2_3_1.py
@@ -1,27 +1,35 @@
 import datetime as dt
 from typing import Callable
+import requests
+
 
 def fabric_logger_func(log_path):
     def logger_func(func: Callable):
         def log_file(*args, **kwargs):
+            result = func(*args, **kwargs)
             with open(log_path, 'w') as f:
                 f.write(str(dt.datetime.today()))
                 f.write('\n')
                 f.write(f'функция {func.__name__}')
                 f.write('\n')
                 f.write(f'аргументы {args} {kwargs}')
+                f.write('\n')
+                f.write(f'возвращает {result}')
             result = func(*args, **kwargs)
             return result
         return log_file
     return logger_func
 
 @fabric_logger_func(log_path = 'log.txt')
-def rec(a, c = 0):
-  b = {}
-  if c < len(a):
-    b[a[c]] =  rec(a, c = c + 1)
-  return b
-rec(a = ['2018-01-01', 'yandex', 'cpc', 100])
+def list_to_dict(*args):
+    li = args
+    if len(li) > 0:
+      di = {li[-1]}
+      for item in li[-2:-len(li)-1 :-1]:
+        di = {item : di}
+    return di
+
+list_to_dict('2018-01-01', 'yandex', 'cpc', 100)
 
 
 
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from typing import Callable
 
-def print_dec(old_func):
+def print_dec(old_func: Callable):
     def new_func(*args, **kwargs):
         print(f'вызвана {old_func.__name__}')
         print(f'c арг {args}, {kwargs}')
-        # result = old_func(*args, **kwargs)
-        # print(f'получили {result}')
-        # return result
-    return new_func
+        result = old_func(*args, **kwargs)
+        print(f'получили {result}')
+        return result
+    return new_func()
 
 @print_dec
 def mult(a, b):
